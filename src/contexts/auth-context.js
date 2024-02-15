0c5fa43dc59adec5b8f2eb389f76d7443e93c007
dcode333 <98689944+dcode333@users.noreply.github.com>
2024-02-16 03:10:01 +0500

    Added admin profile check

--- src/contexts/auth-context.js
+++ src/contexts/auth-context.js
@@ -152,10 +152,12 @@ export const AuthProvider = (props) => {
 
   const signIn = async (userInfo) => {
     const { Token: authToken, user } = userInfo;
+  
 
-    if (!user?.is_superuser && !user?.is_staff) {
-      throw new Error('You are not allowed to Login');
-    }
+    if (!user?.profile) throw new Error('You are not allowed to Login !');
+
+    if (!user?.is_superuser && !user?.is_staff) throw new Error('You are not allowed to Login');
+
 
     try {
       window.sessionStorage.setItem('authenticated', authToken);
